de311b0d711dc714b5d5f443d1af42aef51db3d4
xakim90 <xakim90@gmail.com>
2022-03-02 19:31:20 +0500

    added react-router

--- src/components/AntResponsiveLayout.js
+++ src/components/AntResponsiveLayout.js
@@ -1,7 +1,33 @@
 import { Layout, Menu } from 'antd';
 import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Link, Routes,Route } from 'react-router-dom';
+import Profile from './Profile';
+
 
 const { Header, Content, Footer, Sider } = Layout;
+
+const routes = [
+    {
+        name: "Home",
+        url: "/home",
+        icon: <UserOutlined />
+    },
+    {
+        name: "Profile",
+        url: "/profile",
+        icon: <VideoCameraOutlined />
+    },
+    {
+        name: "Works",
+        url: "/works",
+        icon: <UploadOutlined />
+    },
+    {
+        name: "Register",
+        url: "/register",
+        icon: <UserOutlined />
+    },
+]
 
 const AntLayout = () => {
     return (
@@ -18,28 +44,27 @@ const AntLayout = () => {
             >
                 <div className="logo" />
                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-                    <Menu.Item key="1" icon={<UserOutlined />}>
-                        nav 1
-                    </Menu.Item>
-                    <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                        nav 2
-                    </Menu.Item>
-                    <Menu.Item key="3" icon={<UploadOutlined />}>
-                        nav 3
-                    </Menu.Item>
-                    <Menu.Item key="4" icon={<UserOutlined />}>
-                        nav 4
-                    </Menu.Item>
+                    {
+                        routes.map((item, i) => {
+                            return (
+                                <Menu.Item key={i + item.name} icon={item.icon} >
+                                    <Link to={item.url}>{item.name}</Link>
+                                </Menu.Item>
+                            )
+                        })
+                    }
                 </Menu>
             </Sider>
             <Layout>
                 <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
                 <Content style={{ margin: '24px 16px 0' }}>
                     <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                        content
+                        <Routes>
+                            <Route path="profile" element={<Profile />} />
+                        </Routes>
                     </div>
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                <Footer style={{ textAlign: 'center' }}>Toshkent viloyati Bo'stonliq tuman Prokuraturasi tashabbusi bilan ishlab chiqilgan!</Footer>
             </Layout>
         </Layout>
     )
