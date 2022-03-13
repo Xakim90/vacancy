458d5969698382d7900ac528d30f85a87f261d83
xakim90 <xakim90@gmail.com>
2022-03-13 15:14:31 +0500

    xakim90

--- src/components/DynamicForm.js
+++ src/components/DynamicForm.js
@@ -1,38 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
-import { Input, Select, Button } from 'antd';
+import { Input, Select, Row, Col } from 'antd';
 import CustomButton from "./CustomButton";
 
+const fields = [
+    {
+        name: "firstName",
+        type: "input",
+        label: "Ism",
+        required: true,
+    },
+    {
+        name: "Select",
+        type: "select",
+        label: "Tanlang",
+        required: true,
+        options: [
+            { value: "chocolate", label: "Chocolate" },
+            { value: "strawberry", label: "Strawberry" },
+            { value: "vanilla", label: "Vanilla" }
+        ]
+    },
+]
+
 const DynamicForm = () => {
-    const { control, handleSubmit } = useForm({
+    const { register,control, handleSubmit, watch, formState: { errors } } = useForm({
         defaultValues: {
-            firstName: '',
+            firstName: 'asdasd',
             select: {}
         }
     });
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data)
+    };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <Controller
-                name="firstName"
-                control={control}
-                render={({ field }) => <Input {...field} />}
-            />
-            <Controller
-                name="select"
-                control={control}
-                render={({ field }) => <Select
-                    {...field}
-                    options={[
-                        { value: "chocolate", label: "Chocolate" },
-                        { value: "strawberry", label: "Strawberry" },
-                        { value: "vanilla", label: "Vanilla" }
-                    ]}
-                />}
-            />
-            <CustomButton type="submit" text="Submit" />
-        </form>
+        <div className={fields.length / 4 <= 1 ? "flex justify-center" : ""}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div >
+                    <Row gutter={[16, 16]}>
+                        {
+                            fields.map((f, i) => (
+                                <Col
+                                    span={fields.length / 4 <= 1 ? 24 : fields.length / 4 <= 2 ? 12 : fields.length / 4 <= 3 ? 8 : 6}
+                                    key={f.name + i}>
+                                    <Controller
+                                        key={f.name + i}
+                                        name={f.name}
+                                        control={control}
+                                        render={({ field }) => {
+                                            switch (f.type) {
+
+                                                case "input":
+                                                    return (
+                                                        <>
+                                                            <label>{f.label}</label>
+                                                            <Input {...field} />
+                                                        </>
+                                                    )
+
+
+                                                case "select":
+                                                    return (
+                                                        <>
+                                                            <label>{f.label}</label>
+                                                            <Select
+                                                                className="w-full"
+                                                                {...field}
+                                                                options={f.options.map((option, i) => (
+                                                                    { ...option }
+                                                                ))}
+                                                            />
+                                                        </>
+                                                    )
+                                                default:
+                                                    return null
+                                            }
+                                        }
+                                        }
+                                    />
+                                </Col>
+                            ))
+                        }
+                    </Row>
+                </div>
+                <div className="flex justify-center mt-2">
+                    <div>
+                        <CustomButton className="glow-on-hover w-full" type="submit" text="Submit" />
+                    </div>
+                </div>
+            </form >
+        </div>
     );
 };
 export default DynamicForm;
